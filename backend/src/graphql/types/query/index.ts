08d5d26b0fe8ce1068631f056d732cfdd4a5c09f
Artem Karpov <artem.kar2003@gmail.com>
2025-06-04 14:11:21 +0300

    Add queries for all entities

--- backend/src/graphql/types/query/index.ts
+++ backend/src/graphql/types/query/index.ts
@@ -1,20 +1,49 @@
-import { QueryResolvers } from "../../../generated/schema";
+import { Drone, QueryResolvers } from "../../../generated/schema";
 
 export const queryResolvers: QueryResolvers = {
-    drones: async (_, __, { prisma }) => {
-        const drones = await prisma.drone.findMany();
-        return drones.map(drone => ({
-            ...drone,
-            createdAt: drone.createdAt.toISOString()
-        }));
-    },
+    drones: (_, __, { prisma }) => prisma.drone.findMany(),
     drone: async (_, { id }, { prisma }) => {
         const drone = await prisma.drone.findUnique({ where: { id } });
         if (!drone) throw new Error('Drone not found');
-        return {
-            ...drone,
-            createdAt: drone.createdAt.toISOString()
-        };
+        return drone;
+    },
+    droneModels: (_, __, { prisma }) => prisma.droneModel.findMany(),
+    droneModel: async (_, { id }, { prisma }) => {
+        const model = await prisma.droneModel.findUnique({ where: { id } });
+        if (!model) throw new Error('Drone model not found');
+        return model;
+    },
+    droneSessions: (_, __, { prisma }) => prisma.droneSession.findMany(),
+    droneSession: async (_, { id }, { prisma }) => {
+        const session = await prisma.droneSession.findUnique({
+            where: { id },
+        });
+        if (!session) throw new Error('Session not found');
+        return session;
+    },
+    operators: (_, __, { prisma }) => prisma.operator.findMany(),
+    operator: async (_, { id }, { prisma }) => {
+        const operator = await prisma.operator.findUnique({
+            where: { id },
+        });
+        if (!operator) throw new Error('Operator not found');
+        return operator;
+    },
+    positions: (_, __, { prisma }) => prisma.position.findMany(),
+    position: async (_, { id }, { prisma }) => {
+        const position = await prisma.position.findUnique({
+            where: { id }
+        });
+        if (!position) throw new Error('Position not found');
+        return position;
+    },
+    regions: (_, __, { prisma }) => prisma.region.findMany(),
+    region: async (_, { id }, { prisma }) => {
+        const region = await prisma.region.findUnique({
+            where: { id },
+        });
+        if (!region) throw new Error('Region not found');
+        return region;
     },
 }
 

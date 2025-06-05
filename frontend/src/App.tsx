fdda9f2f59eebfd5fdf10b7c5c7a93182b57e066
Artem Karpov <artem.kar2003@gmail.com>
2025-06-05 07:21:06 +0300

    Create frontend with chakra-UI installed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Box, Button, Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box p={4}>
+      <Text fontSize="xl">Hello, Chakra UI + React + TypeScript!</Text>
+      <Button mt={4} colorScheme="teal">Click me</Button>
+    </Box>
+  );
+}
+
+export default App;
